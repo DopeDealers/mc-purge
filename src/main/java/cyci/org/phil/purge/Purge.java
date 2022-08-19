199a37ef2f86f1d588d9a28ae19723759c7dc046
Phil - DopeDealers <dopedealersdev@gmail.com>
2022-08-18 22:06:40 -0400

    Initial commit

---
+++ src/main/java/cyci/org/phil/purge/Purge.java
@@ -0,0 +1,17 @@
+package cyci.org.phil.purge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Purge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
